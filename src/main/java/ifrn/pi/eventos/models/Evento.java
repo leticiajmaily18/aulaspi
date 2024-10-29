780c42105138c03bfea782522f6e683753d3a1de
leticiajmaily18 <leticia.jamily@escolar.ifrn.edu.br>
2024-10-29 10:21:32 -0300

    Criando Repository para Evento

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -12,8 +12,8 @@ public class Evento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
-	private String local;
-	private String data;
+	private String localF;
+	private String dataF;
 	private String horario;
 	
 	public Long getId() {
@@ -33,19 +33,19 @@ public class Evento {
 	}
 
 	public String getLocal() {
-		return local;
+		return localF;
 	}
 
 	public void setLocal(String local) {
-		this.local = local;
+		this.localF = local;
 	}
 
 	public String getData() {
-		return data;
+		return dataF;
 	}
 
 	public void setData(String data) {
-		this.data = data;
+		this.dataF = data;
 	}
 
 	public String getHorario() {
@@ -58,7 +58,7 @@ public class Evento {
 
 	@Override
 	public String toString() {
-		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + localF + ", data=" + dataF + ", horario=" + horario
 				+ "]";
 	}
 	
