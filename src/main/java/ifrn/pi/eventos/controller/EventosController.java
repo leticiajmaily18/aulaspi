162f693f475453453bcb37e1d23daa2c88479824
leticiajmaily18 <leticia.jamily@escolar.ifrn.edu.br>
2024-10-29 10:01:54 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/ifrn/pi/eventos/controller/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controller/EventosController.java
@@ -3,7 +3,6 @@ package ifrn.pi.eventos.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import ifrn.pi.eventos.models.Evento;
 
@@ -15,13 +14,11 @@ public class EventosController {
         return "formEvento";
     }
 
-    @PostMapping("/eventos/submeter")
-    public String submetido(Evento evento) {
-
-    	 System.out.println("Nome: " + evento.getNome());
-         System.out.println("Local: " + evento.getLocal());
-         System.out.println("Data: " + evento.getData());
-         System.out.println("Horário: " + evento.getHorario());
-    	return "formSubmetido";
+    @PostMapping("/eventos")
+    public String adicionar(Evento evento) {
+    	
+    	System.out.println(evento);
+    	
+    	return "evento-adicionado";
     }
 }
